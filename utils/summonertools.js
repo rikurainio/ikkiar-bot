520d73dfa39c39b28674d005581842d322eaedb9
Riku Rainio <rkurainio@gmail.com>
2022-04-28 01:23:53 +0300

    ikkiardb summoners now gain/lose points on match submit

--- utils/summonertools.js
+++ utils/summonertools.js
@@ -6,9 +6,13 @@ const handleSummonerUpdatesAfterMatch = async (mongoMatch) => {
     const { metadata, teams, summoners } = mongoMatch
     const { participants: puuids } = metadata
 
-    summoners.forEach(async (summoner, idx) => {
-        // tie context
+    // COMPARE TEAMS THAT PLAYED
+    const blueTeam = getBlueTeam(summoners)
+    const redTeam = getRedTeam(summoners)
+    const betterTeam = calculateTeamSkillDifference(blueTeam, redTeam)
+    //const fx = Math.floor(Math.random() * 1)
 
+    summoners.forEach(async (summoner, idx) => {
         // we need to create a new summoner document if its his/her first game in ikkiar
         // puuids and summoners are in same order so their indexes SHOULD match
         const found = await findSummonerByPuuId(puuids[idx])
@@ -27,12 +31,22 @@ const handleSummonerUpdatesAfterMatch = async (mongoMatch) => {
 
         // reward wins
         if(summoner.win){
-
+            if(summoner.teamId == betterTeam){
+                updateSummonerStats(summoner.puuid, 20, true)
+            }
+            else{
+                updateSummonerStats(summoner.puuid, 21, true)
+            }
         }
 
         // punish loss
         if(!summoner.win){
-
+            if(summoner.teamId == betterTeam){
+                updateSummonerStats(summoner.puuid, 21, false)
+            }
+            else{
+                updateSummonerStats(summoner.puuid, 20, false)
+            }
         }
     })
 }
@@ -47,13 +61,50 @@ const getRedTeam = (summoners) => {
     return redTeam
 }
 
-const calculateTeamSkillDifference = async (blueTeam, redTeam) => {
 
+// returns which team was better
+const calculateTeamSkillDifference = async (blueTeam, redTeam) => {
+    let bluePoints = 0
+    let redPoints = 0
+
+    blueTeam.forEach(async blueSummoner => {
+        let bluePoint = await getPointsByPuuId(blueSummoner.puuid)
+        console.log('bluepoint: ', bluePoint)
+        bluePoints += bluePoint
+    })
+    redTeam.forEach(async redSummoner => {
+        let redPoint = await getPointsByPuuId(redSummoner.puuid)
+        console.log('redpoint:', redPoint)
+        redPoints += redPoint
+    })
+
+    // blue was "better team" points wise
+    if(bluePoints > redPoints){
+        return 100
+    }
+    // red was "better"
+    else{
+        return 200
+    }
 }
 
 const getSummoners = async () => {
     const summoners = await Summoner.find({})
     return summoners
+}
+
+const updateSummonerStats = async (puuid, pointsAmount, win) => {
+    if(win){
+        await Summoner.findOneAndUpdate({puuid: puuid}, {$inc : { points: pointsAmount, wins: 1 }})
+    }
+    else{
+        await Summoner.findOneAndUpdate({puuid: puuid}, {$inc : { points: -pointsAmount, losses: 1 }})
+    }
+}
+
+const getPointsByPuuId = async (puuid) => {
+    const summoner = await Summoner.findOne({ puuid: puuid})
+    return summoner.points
 }
 
 const resetSummoners = async () => {
@@ -69,8 +120,8 @@ const deleteSummoner = async (puuid) => {
     await Summoner.findOneAndRemove({puuid: puuid})
 }
 
-const updateSummoner = async (update) => {
-    await Summoner.findOneAndUpdate({puuid: puuid}, {update})
+const updateSummoner = async (puuid, update) => {
+    await Summoner.findOneAndUpdate({puuid: puuid}, update)
 }
 
 const findSummonerByPuuId = async (puuid) => {
