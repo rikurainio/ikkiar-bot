dd6db05be5b41ec3f86617bb340f00486aeb7d52
Veli Kivilahti <veli.kivilahti@gmail.com>
2022-05-08 13:11:18 +0300

    formatting test

--- utils/matchtest.js
+++ utils/matchtest.js
@@ -1,19 +1,17 @@
-
 var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream("./utils/r.rofl"),
-  });
-  var lineCounter = 0; var match = [];
-  lineReader.on('line', function (line) {
+});
+var lineCounter = 0; var match = [];
+lineReader.on('line', function (line) {
     lineCounter++;
     if (lineCounter === 2) match.push(line);
-    if(lineCounter===2){lineReader.close();}
-  });
-  lineReader.on('close', function() {
+    if (lineCounter === 2) { lineReader.close(); }
+});
+lineReader.on('close', function () {
     match = match.toString();
-    match = match.substring(match.indexOf('{'),match.indexOf('}]"}')) + '}]"}'
+    match = match.substring(match.indexOf('{'), match.indexOf('}]"}')) + '}]"}'
     //console.log(match);
     var json = JSON.parse(match);
     console.log(json);
     process.exit(0);
-
-  });
+});
